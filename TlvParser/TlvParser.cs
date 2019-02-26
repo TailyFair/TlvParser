2c480fa4d2c1fd2dd8766acd57a632c4056ad550
TailyFair <karolis.kalzanauskas@stud.vgtu.lt>
2019-02-26 11:52:47 +0200

    Binary reader added

--- TlvParser/TlvParser.cs
+++ TlvParser/TlvParser.cs
@@ -1,12 +1,23 @@
 using System;
+using System.IO;
 using System.Collections.Generic;
 
 namespace TlvParser
 {
     public class Parser
     {
-        public Tlv[] Parse(byte[] input_bytes) {
+        public Tlv[] Parse(byte[] input_bytes)
+        {
             List<Tlv> tlvs = new List<Tlv>();
+
+            using (MemoryStream stream = new MemoryStream(input_bytes))
+            using (BinaryReader reader = new BinaryReader(stream))
+            {
+                while ((stream.Length - stream.Position) > 0)
+                {
+                    reader.ReadByte();
+                }
+            }
 
             return tlvs;
         }
